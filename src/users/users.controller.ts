a1865b2b941fb88167aa0d66f0e88d8c93a7716f
Sabino Alejandro Arambula Rodriguez <arambularodriguezsabino@gmail.com>
2025-03-13 21:32:35 -0600

    Validation and Pipes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,37 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { GetUsersParamDto } from './dtos/get-users-param.dto';
+import { PatchUserDto } from './dtos/patch-user.dto';
 
 @Controller('users')
 export class UsersController {
-  @Get('/:id')
-  public getUsers(@Param('id') id: number) {
-    return `getUsers lol ${id}`;
+  @Get('/:id?')
+  public getUsers(
+    @Param() params: GetUsersParamDto,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    return `getUsers lol ${params.id}, ${limit}, ${page}`;
   }
 
   @Post()
-  public createUsers(@Body() request: any) {
-    console.log(request);
+  public createUsers(@Body() body: CreateUserDto) {
+    console.log(body);
     return 'post create';
+  }
+
+  @Patch()
+  public patchUser(@Body() body: PatchUserDto) {
+    console.log(body);
   }
 }
